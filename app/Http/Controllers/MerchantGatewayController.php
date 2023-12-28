5bc9fa9a8a36a225ce8f7e5447aeaab5d6fe5609
Vladislav <vtinishov@gmail.com>
2023-12-29 02:18:39 +0600

    create payment migration, model and controllers

---
+++ app/Http/Controllers/MerchantGatewayController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repository\PaymentRepository;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class MerchantGatewayController extends Controller
+{
+    //
+}
